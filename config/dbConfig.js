eb422afa2fcd429ea3c5ae5ddc26ee8aff65473c
Josh Mendoza <jjm121197@gmail.com>
2022-01-19 14:23:39 -0600

    db config change

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,9 +1,9 @@
 // store the configuration secrets of the PG DB here
 module.exports = {
-    HOST: 'ec2-3-227-15-75.compute-1.amazonaws.com', // your endpoint
-    USER: 'yonvzitoefcsmr', // your username
-    PASSWORD: '7500f634879beed9486b33b2be3bb2e05ea5a7935b351444a374eb9087fbbc38', // your password
-    DB: 'dda637hs24720g', // your db name
+    HOST: 'ec2-3-216-113-109.compute-1.amazonaws.com', // your endpoint
+    USER: 'zcqgwllxwsiddp', // your username
+    PASSWORD: '237684f27f5b77a86b41c8b6bc76eab675356518ef56f8649af3d4b6b934ba75', // your password
+    DB: 'd3fb24o5gjvjl', // your db name
     dialect: 'postgres',
     }
 
